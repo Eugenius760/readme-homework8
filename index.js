315f67ad235c751cc3fd223bcb851a83bffa844b
Darryl <williams.de39@gmail.com>
2020-04-29 17:14:27 -0700

    added requires

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util")
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "project-name"
+    },
+    {
+        type: "input",
+        message: "Tell me about your project",
+        description: "description",
+    },
+]);
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
