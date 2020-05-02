a21a6f9268c51a8558b39c57461c4fc47ba7486f
Darryl <williams.de39@gmail.com>
2020-05-01 21:02:48 -0700

    functions to generate Readme1 file

--- index.js
+++ index.js
@@ -1,53 +1,84 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util")
+const markDown = require("./utils/generateMarkdown")
+
+const writeToFile = util.promisify(fs.writeFile);
 
 
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Tell me about your project",
+            name: "description",
+        },
+        {
+            type: "list",
+            message: "What did you use for install?",
+            name: "install",
+            choices: [
+                "npm i",
+                "other",
+            ]
+        },
+        {
+            type: "list",
+            message: "What license did you use?",
+            name: "license",
+            choices: [
+                "MIT",
+                "GNU AGPLv3",
+                "Apache 2.0",
+                "The Unlicense",
+            ]
+        },
+        {
+            type: "input",
+            message: "Who contributed to the project? (add github username)",
+            name: "contributers"
+        }
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the name of your project?",
-        name: "project-name"
-    },
-    {
-        type: "input",
-        message: "Tell me about your project",
-        name: "description",
-    },
-    {
-        type: "list",
-        message: "What did you use for install?",
-        name: "install",
-        choices: [
-            "npm i",
-            "other",
-        ]
-    },
-    {
-        type: "list",
-        message: "What license did you use?",
-        name: "license",
-        choices: [
-            "MIT",
-            "GNU AGPLv3",
-            "Apache 2.0",
-            "The Unlicense",
-        ]
-    },
-    {
-        type: "input",
-        message: "Who contributed to the project? (add github username)",
-        name: "contributers"
-    }
-
-]);
-
-function writeToFile(fileName, data) {
+    ]);
 }
 
-function init() {
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  ## ${data.description}
+  ## ${data.install}
+  ## ${data.license}
+  ## ${data.contributers}
+  
+  `;
+  }
 
-}
+  promptUser()
+    .then(function(data) {
+        const readme = generateMarkdown(data);
 
-init();
+        return writeToFile("README1.md", readme)
+    })
+    .then(function() {
+        console.log("Successfully wrote to index.html");
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+  
+  module.exports = generateMarkdown;
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
